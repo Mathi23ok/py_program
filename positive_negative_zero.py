771ad73fd64937d6d0199565a538e79846237749
Sanjana Joseph <164984683+Mathi23ok@users.noreply.github.com>
2025-02-23 11:37:53 +0530

    Create positive_negative_zero.py

---
+++ positive_negative_zero.py
@@ -0,0 +1,10 @@
+#positive or negative or zero 
+n = int(input("Enter a number:"))
+if(n>0):
+    print("positive")
+elif n<0:
+    print("negative")
+elif n==0:
+    print("zero")
+else:
+    print("invalid")
